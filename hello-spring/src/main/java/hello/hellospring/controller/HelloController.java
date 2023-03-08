15519025ebf6f441a2d8ef470e08d69ea8b828a0
yerin03 <kyr2532@naver.com>
2023-03-09 00:12:23 +0900

    MVC and API

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,4 +16,42 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //API)ResponseBody 문자반환
+    @GetMapping("hello-string")
+    @ResponseBody //viewResolver를 사용하지 않음
+    public String helloString(@RequestParam("name")String name){
+        return "hello"+name;
+    }
+    //문자 그 자체가 반환됨 소스보기 봐보셈 ~
+
+    //API)ResponseBody 객체반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+    }
+    //json방식으로 {"name";"yell"} 이케 나옴 아하 신기 ~
+    //view를 사용 안한다는 
 }
